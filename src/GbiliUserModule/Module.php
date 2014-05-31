f9002dea5241fccd6aedc552fa86346ba112d132
Guillermo Devi <g@gbili.com>
2014-05-31 18:31:00 +0200

    extracted doctrine injection and entities to other modules

--- src/GbiliUserModule/Module.php
+++ src/GbiliUserModule/Module.php
@@ -33,44 +33,5 @@ class Module
                 $e->getViewModel()->setVariable('successMessages', $flashMessenger->getSuccessMessages());
             }
         });
-
-        $this->injectDoctrineTargetListeners($e);
-    }
-
-    public function injectDoctrineTargetListeners($e)
-    {
-        $sm = $e->getApplication()->getServiceManager();
-
-        $config = $sm->get('Config');
-        $doctrineEventListenersConfig = $config['doctrine_event_listeners'];
-
-        $em = $sm->get('doctrine.entitymanager.orm_default');
-        $dem = $em->getEventManager();
-
-        $addedEventListenerHashesToPriority = array();
-
-        foreach ($doctrineEventListenersConfig as $eventIdentifier => $eventListeners) {
-            foreach ($eventListeners as $eventListenerSet) {
-                $listenerClass = $eventListenerSet['listener_class'];
-                $listenerMethod = $eventListenerSet['listener_method'];
-                foreach ($eventListenerSet['listeners_params'] as $listenerIdentifierPart => $listenerParams) {
-                    $listenerHash = md5($eventIdentifier . $listenerClass . $listenerMethod . $listenerIdentifierPart);
-                    $listenerPriority = (isset($eventListenerSet['priority']))
-                        ? $eventListenerSet['priority']
-                        : 0;
-                    if (isset($addedEventListenerHashesToPriority[$listenerHash])) {
-                        $lastPriority = $addedEventListenerHashesToPriority[$listenerHash];
-                        if ($lastPriority >= $listenerPriority) continue;
-                    }
-
-                    $listener = new $listenerClass;
-                    call_user_func_array(array($listener, $listenerMethod), $listenerParams);
-                    // Last added takes priority over the rest
-                    $dem->addEventListener($eventIdentifier, $listener);
-
-                    $addedEventListenerHashes[$listenerHash] = $listenerPriority;
-                }
-            }
-        }
     }
 }
