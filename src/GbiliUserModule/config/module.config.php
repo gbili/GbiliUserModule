53aeb53cbe799b5824b9344df670dfb2827c4f85
Guillermo Devi <g@gbili.com>
2014-06-02 17:54:43 +0200

    doctrine event listneres config

--- src/GbiliUserModule/config/module.config.php
+++ src/GbiliUserModule/config/module.config.php
@@ -10,6 +10,7 @@ return array(
     'controller_plugins'       => include __DIR__ . '/controller_plugins.config.php',
     'controllers'              => include __DIR__ . '/controllers.config.php',
     'doctrine'                 => include __DIR__ . '/doctrine.config.php',
+    'doctrine_event_listeners' => include __DIR__ . '/doctrine_event_listeners.config.php',
     'navigation'               => include __DIR__ . '/navigation.config.php', 
     'router'                   => include __DIR__ . '/router.config.php',
     'service_manager'          => include __DIR__ . '/service_manager.config.php',
